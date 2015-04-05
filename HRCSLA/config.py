e802adf9b5bc3325c8de0223e0cee67cb44cbd7d
Matt <mj1@andreini.us>
2015-04-05 20:47:56 +0200

    HRCSLA V2.0.1
    This is the first in the new system designed with a client-side webapp
    to process the reports in the database and can easily be configured for
    any network due to config.py

---
+++ HRCSLA/config.py
@@ -0,0 +1,15 @@
+"""
+Created on March 2015
+@author: ma56nin / Matt
+Contact: ma56nin@hotmail.com
+"""
+
+""" Config """
+
+# Slacker Oauth
+
+#REMOVED FOR SECURITY PURPOSES
+
+# MySQL
+
+#REMOVED FOR SECURITY PURPOSES
